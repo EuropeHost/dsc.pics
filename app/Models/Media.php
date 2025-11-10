519d8ff4e6e3ff4f92211427a8eb0652fea26961
Michaelninder <fabianeternis@gmail.com>
2025-11-10 15:25:47 +0100

    Image => Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
-class Image extends Model
+class Media extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id', 'type', 'filename', 'original_name', 'mime', 'size', 'is_public', 'slug',
